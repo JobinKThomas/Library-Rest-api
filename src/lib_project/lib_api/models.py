a924c4458d69b0d27378ed6cf64464876251e20b
jobin <jobinkthomas007@gmail.com>
2020-10-18 12:03:01 +0530

    Create model and migration

--- src/lib_project/lib_api/models.py
+++ src/lib_project/lib_api/models.py
@@ -1,6 +1,17 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
